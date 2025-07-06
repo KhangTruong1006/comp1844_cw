58fc8abea22d26d15214d7474094c91404b79f49
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2025-07-06 16:24:46 +0700

    new functions for labeling names, and reorganise functions

--- coursework_tube_map.py
+++ coursework_tube_map.py
@@ -4,27 +4,43 @@ import networkx as nx
 import matplotlib.pyplot as plt
 
 import TubeSystem
+from Setting import Setting
 
 class TubeMap():
     def __init__(self):  
         """Settings"""
+        self.settings = Setting()
+
         self.lineStations = TubeSystem.LineStation()
         self.direction = TubeSystem.Direction()
         self.lineColor = TubeSystem.LineColor()
         self.stationDistance = TubeSystem.StationDistance()
 
-        self.angle = np.radians(45)
-        self.distance = 2
+        self.angle = np.radians(self.settings.angle)
+        self.distance = self.settings.distance
 
         self.tubeGraph = nx.Graph()
 
     """Functions"""
+    # Edge generator functions
     def createEdges(self,lineStaions):
         edges = []
         for i in range(len(lineStaions) - 1):
             edges.append((lineStaions[i],lineStaions[i+1]))
         return edges
     
+    def generateEdgeColor(self,lineStation):
+        edges = self.createEdges(lineStation)
+        edge_colors = []
+        for edge in self.tubeGraph.edges():
+            if edge in edges:
+                edge_colors.append("blue")
+
+            else:
+                edge_colors.append("gray")
+        return edge_colors
+
+    # Position generator functions
     def generatePosition(self,direction_list,start_pos= (0,0)):
         pos = []
         for direction in direction_list:
@@ -35,51 +51,73 @@ class TubeMap():
                 x,y = x, y+ self.distance
 
             elif direction == "NE":
-                x,y = x + self.distance * np.sin(self.angle) , y + self.distance*np.sin(self.angle)
+                x,y = x + self.distance * np.cos(self.angle) , y + self.distance*np.sin(self.angle)
 
             elif direction == "E":
                 x,y = x + self.distance, y
 
             elif direction == "SE":
-                x,y = x + self.distance * np.sin(self.angle) , y - self.distance*np.sin(self.angle)
+                x,y = x + self.distance * np.cos(self.angle) , y - self.distance*np.sin(self.angle)
 
             elif direction == "S":
                 x,y = x, y - self.distance
 
             elif direction == "SW":
-                x,y = x - self.distance * np.sin(self.angle) , y - self.distance*np.sin(self.angle)
+                x,y = x - self.distance * np.cos(self.angle) , y - self.distance*np.sin(self.angle)
 
             elif direction == "W":
                 x,y = x - self.distance, y 
 
             elif direction == "NW":
-                x,y = x + self.distance - np.sin(self.angle) , y + self.distance*np.sin(self.angle)
+                x,y = x + self.distance * np.cos(self.angle) , y + self.distance*np.sin(self.angle)
             
             pos.append((x,y))
 
         return pos
             
-    def generateStationPosition(self,stations,station_direction):
+    def generateStationPosition(self,lineStations,station_direction):
         pos ={}
         stationPos = self.generatePosition(station_direction)
-        for i in range(len(stations)):
-            pos[f'{stations[i]}'] = stationPos[i]
+        for i in range(len(lineStations)):
+            pos[f'{lineStations[i]}'] = stationPos[i]
 
         return pos
     
+    # Color generator functions
     def generateLineColorLegend(self):
         for line in self.lineColor.list:
             plt.plot([], [], color=line[0], linewidth=2, label=line[1])
 
+    def generateNodeColorList(self,lineStations,lineColor):
+        node_color_list = []
+        for i in range(len(lineStations)):
+            node_color_list.append(lineColor[0])
+        return node_color_list
+    
+    # Others
+    def labelStationNames(self,lineStations, linePos):
+        for station in lineStations:
+            (x,y) = linePos[station]
+            plt.text(x - 1, y + 0.2 , station, ha= 'center', va='bottom',fontsize = self.settings.fontSize)
     
     """Graph"""
-    def drawTubeMap(self):
-        
-        piccadilly_line_edges = self.createEdges(self.lineStations.piccadilly)
-        self.tubeGraph.add_edges_from(piccadilly_line_edges)
-        piccadillyPos = self.generateStationPosition(self.lineStations.piccadilly,self.direction.piccailly)
-        
-        nx.draw(self.tubeGraph, piccadillyPos, node_color= self.lineColor.piccadilly[0])
+    def createLine(self,lineStations,lineDirection,lineColor):
+        line_edges = self.createEdges(lineStations)
+        self.tubeGraph.add_edges_from(line_edges)
+        pos = self.generateStationPosition(lineStations,lineDirection)
+        stationColorList = self.generateNodeColorList(lineStations,lineColor)
+        self.labelStationNames(lineStations,pos)
+
+        return (pos,stationColorList)
+    
+
+    def drawTubeMap(self):  
+        pos, nodeColorList = self.createLine(self.lineStations.piccadilly,self.direction.piccailly,self.lineColor.piccadilly)
+        edge_colors = self.generateEdgeColor(self.lineStations.piccadilly)
+        nx.draw(self.tubeGraph, 
+                pos, 
+                node_color= nodeColorList,
+                edge_color = edge_colors)
 
         self.generateLineColorLegend()
         plt.title('London Tube Map')
