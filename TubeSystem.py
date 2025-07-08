16c7708a8d5dc06c82ba697a1ef9664727c2d7fa
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2025-07-08 13:34:16 +0700

    new line name Central line

--- TubeSystem.py
+++ TubeSystem.py
@@ -4,23 +4,33 @@ class TubeSystem:
             "Piccadilly",
             "blue",
             ["Hyde Park Corner","Green Park","Piccadilly Circus","Leicester Square","Covent Garden","Holborn"],
-            [False,False,False,False,False,False],
+            [False,False,False,False,False,True],
             ["Start","NE","E","E","NE","NE"],
             ["r","t","t","b","r","l"],
             [0.81,0.7,0.45,0.33,0.6]
         )
 
-        self.list = [self.piccadilly]
+        self.central = self.lineData(
+            "Central",
+            "red",
+            ["Holborn","Tottenham Court Road","Oxford Circus","Bond Street","Marble Arch","Lancaster Gate"],
+            [True,True,True,True,False,False],
+            ["Start","W","W","W","SW","SW"],
+            ["r","t","t","b","r","l"],
+            ["a","a","a","a","a"]
+        )
+
+        self.lines = [self.piccadilly,self.central]
 
     def lineData(self,key_name,color,station,interchange,direction,placement,distance):
         data ={
             "key" : key_name,
             "color" : color,
-            "station" : station,        #Station name
-            "interchange": interchange, #Interchange Station
-            "direction" : direction,    #Station direction
-            "placement" : placement,    #Placement of station name
-            "distance" : distance
+            "station" : station,        # Station name
+            "interchange": interchange, # Interchange Station
+            "direction" : direction,    # Station direction
+            "placement" : placement,    # Placement of station name
+            "distance" : distance       # Distance between stations
         }
         
         return data
