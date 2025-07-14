ef6130395e21d34e4c1d5f4ffbc0d627bdb70675
KhangTruong1006 <156401203+KhangTruong1006@users.noreply.github.com>
2025-07-14 14:45:44 +0700

    add placeholder for station node

--- TubeSystem.py
+++ TubeSystem.py
@@ -3,6 +3,7 @@ class TubeSystem:
         self.piccadilly = self.lineData(
             "Piccadilly",
             "blue",
+            [1,2,3,4,5,6],
             ["Hyde Park Corner","Green Park","Piccadilly Circus","Leicester Square","Covent Garden","Holborn"],
             [False,False,False,False,False,True],
             ["-","NE","E","E","NE","NE"],
@@ -13,6 +14,7 @@ class TubeSystem:
         self.central = self.lineData(
             "Central",
             "red",
+            [7,8,9,10,11,12,13,14],
             ["Holborn","Tottenham Court Road","Oxford Circus","Bond Street","Marble Arch","Lancaster Gate","Queensway","Notting Hill Gate"],
             [False,False,False,False,False,False,False,True],
             ["-","W","W","W","SW","SW","SW","W"],
@@ -22,10 +24,11 @@ class TubeSystem:
 
         self.lines = [self.piccadilly,self.central]
 
-    def lineData(self,key_name,color,station,interchange,direction,placement,distance):
+    def lineData(self,key_name,color,placeholder,station,interchange,direction,placement,distance):
         data ={
             "key" : key_name,
             "color" : color,
+            "placeholder": placeholder, # Placeholder for station node - id
             "station" : station,        # Station name
             "interchange": interchange, # Interchange Station
             "direction" : direction,    # Station direction
