fc9846d02dab265f33ec996a654a46c87f43e71d
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2025-07-20 18:07:32 +0700

    new stations for both line and adjust some name position

--- TubeSystem.py
+++ TubeSystem.py
@@ -3,23 +3,23 @@ class TubeSystem:
         self.piccadilly = self.lineData(
             "Piccadilly",
             "blue",
-            ["p1","p2","p3","p4","p5","p6"],
-            ["Hyde Park Corner","Green Park","Piccadilly Circus","Leicester Square","Covent Garden","Holborn"],
-            [False,False,False,False,False,True],
-            ["-","NE","E","E","NE","NE"],
-            ["l","t","t","br","r","r"],
-            [0.81,0.7,0.45,0.33,0.6]
+            ["p1","p2","p3","p4","p5","p6","p7","p8"],
+            ["Hyde Park Corner","Green Park","Piccadilly Circus","Leicester Square","Covent Garden","Holborn","Russell Square","King Cross\n& St Pancras\nInternational"],
+            [False,False,False,False,False,True,False,True],
+            ["-","NE","E","E","NE","NE","NE","N"],
+            ["l","t","t","br","r","r","l","r"],
+            [0.81,0.7,0.45,0.33,0.6,"A","A"]
         )
 
         self.central = self.lineData(
             "Central",
             "red",
-            ["c1","c2","c3","c4","c5","c6","c7","c8"],
-            ["Holborn","Tottenham Court Road","Oxford Circus","Bond Street","Marble Arch","Lancaster Gate","Queensway","Notting Hill Gate"],
-            [False,False,False,False,False,False,False,True],
-            ["-","W","W","W","SW","SW","SW","W"],
-            ["r","t","t","t","l","r","b","tl"],
-            ["a","a","a","a","a","a","a"]
+            ["c1","c2","c3","c4","c5","c6","c7","c8","c9","c10"],
+            ["St Paul's","Chancery Lane","Holborn","Tottenham Court Road","Oxford Circus","Bond Street","Marble Arch","Lancaster Gate","Queensway","Notting Hill Gate"],
+            [False,False,True,False,False,False,False,False,False,True],
+            ["-","W","NW","NW","W","W","SW","SW","SW","W"],
+            ["r","b","r","t","b","t","tl","r","br","l"],
+            ["a","a","a","a","a","a","a","a","a"]
         )
 
         self.lines = [self.piccadilly,self.central]
