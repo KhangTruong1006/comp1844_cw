89dcb3fa7df003b447a8c7f55d937f065c3347ae
KhangTruong1006 <truongnhatkhang1006@gmail.com>
2025-07-20 16:42:39 +0700

    new setting for figsize, change offset values and distance

--- Setting.py
+++ Setting.py
@@ -1,17 +1,19 @@
 class Setting:
     def __init__(self):
         """Setting"""
-        self.distance = 1.5
+        self.distance = 10
         self.diagonal_distance = 1
         self.angle = 45
 
         self.fontSize = 8
         self.lineWidth = 2
 
-        self.label_x_offset = 0.3
-        self.label_y_offset = 0.3
+        self.label_x_offset = 1
+        self.label_y_offset = 1
 
         """Diagram Setting"""
+        self.figsize = (12,8) # Width - Height
+
         self.diagram_name = "London Underground Map"
         self.legend_title = "Key"
         self.legend_location = "lower right"
